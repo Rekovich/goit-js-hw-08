1198238f3463e6d2c524103824c945138a5ffecd
Rostyslav Kachor <rostynia@gmail.com>
2022-12-29 20:02:50 +0200

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
+
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+if (localStorage.getItem('videoplayer-current-time')) {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+  console.log(data.seconds);
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
 
